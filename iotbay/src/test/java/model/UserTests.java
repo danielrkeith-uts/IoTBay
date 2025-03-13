a2ca0342383fe6cd47fdbc92a96e33e56f338907
danielrkeith-uts <daniel.r.keith@student.uts.edu.au>
2025-03-13 15:39:15 +1100

    Created user and test

---
+++ iotbay/src/test/java/model/UserTests.java
@@ -0,0 +1,23 @@
+package model;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class UserTests {
+    private User greg;
+    private User james;
+    private User david;
+
+    public UserTests() {
+        greg = new User("Gregory");
+        james = new User("James");
+        david = new User("David");
+    }
+
+    @Test
+    public void testGreeting() {
+        Assert.assertEquals(greg.greeting(), "Hello, Gregory!");
+        Assert.assertEquals(james.greeting(), "Hello, James!");
+        Assert.assertEquals(david.greeting(), "Hello, David!");
+    }
+}
