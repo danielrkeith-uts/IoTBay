362bc9f1b1e7ad5e303c797307ae5ed990f8f19e
danrkeith <daniel.r.keith@student.uts.edu.au>
2025-04-04 14:12:30 +1100

    Added address to order

--- iotbay/src/main/java/model/Order.java
+++ iotbay/src/main/java/model/Order.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Order implements Serializable {
     private List<ProductListEntry> productList;
     private Payment payment;
+    private Address address;
 
-    public Order(List<ProductListEntry> productList, Payment payment) {
+    public Order(List<ProductListEntry> productList, Payment payment, Address address) {
         this.productList = productList;
         this.payment = payment;
+        this.address = address;
     }
 
     public List<ProductListEntry> getProductList() {
@@ -18,5 +20,9 @@ public class Order implements Serializable {
 
     public Payment getPayment() {
         return payment;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 }
