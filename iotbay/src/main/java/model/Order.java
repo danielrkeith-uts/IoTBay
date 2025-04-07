b043924f342826b258c134d0f7c1fc863ff25a5a
danrkeith <daniel.r.keith@student.uts.edu.au>
2025-04-07 14:18:53 +1000

    Added datePlaced to Order

--- iotbay/src/main/java/model/Order.java
+++ iotbay/src/main/java/model/Order.java
@@ -1,17 +1,20 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 public class Order implements Serializable {
     private List<ProductListEntry> productList;
     private Payment payment;
     private Delivery delivery;
+    private Date datePlaced;
 
     public Order(List<ProductListEntry> productList, Payment payment, Delivery delivery) {
         this.productList = productList;
         this.payment = payment;
         this.delivery = delivery;
+        datePlaced = new Date();
     }
 
     public List<ProductListEntry> getProductList() {
@@ -24,5 +27,9 @@ public class Order implements Serializable {
 
     public Delivery getDelivery() {
         return delivery;
+    }
+
+    public Date getDatePlaced() {
+        return datePlaced;
     }
 }
