971ec6a3f1fb3932a18f1203fe25b34e5303d531
danrkeith <daniel.r.keith@student.uts.edu.au>
2025-05-01 15:24:58 +1000

    Added web.xml and removed user-specific file

--- iotbay/src/main/java/model/dao/DB.java
+++
@@ -1,9 +0,0 @@
-package model.dao;
-
-import java.sql.Connection;
-
-public abstract class DB {   
-    protected String URL = "jdbc:sqlite:../database/database.db";
-    protected String driver = "org.sqlite.JDBC"; 
-    protected Connection conn;
-}
