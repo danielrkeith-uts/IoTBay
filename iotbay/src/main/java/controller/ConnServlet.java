2c64edfe85ab4d6ddba3bbb61635fbbcb367f4d3
danrkeith <daniel.r.keith@student.uts.edu.au>
2025-05-02 14:27:34 +1000

    Added ConnServlet

---
+++ iotbay/src/main/java/controller/ConnServlet.java
@@ -0,0 +1,47 @@
+package controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import model.dao.DBConnector;
+import model.dao.UserDBManager;
+
+public class ConnServlet extends HttpServlet {
+    private Logger logger;
+
+    private DBConnector dbConnector;
+    private Connection conn;
+    private UserDBManager userDBManager;
+
+    @Override
+    public void init() {
+        logger = Logger.getLogger(ConnServlet.class.getName());
+
+        try {
+            dbConnector = new DBConnector();
+        } catch (ClassNotFoundException | SQLException e) {
+            logger.log(Level.SEVERE, "Cannot connect to DB", e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+
+        // Instantiate all DBManagers
+        try {
+            conn = dbConnector.openConnection();
+            userDBManager = new UserDBManager(conn);
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Could not instantiate DBManagers", e);
+        }
+        session.setAttribute("userDBManager", userDBManager);
+    }
+}
