3fdef0b2e9ce6c4def491e6d784e6df3cc9766b7
danrkeith <daniel.r.keith@student.uts.edu.au>
2025-05-08 18:35:26 +1000

    Validate phone number tests

--- iotbay/src/test/java/util/ValidatorTests.java
+++ iotbay/src/test/java/util/ValidatorTests.java
@@ -14,11 +14,39 @@ public class ValidatorTests {
         Assert.assertTrue(testValidateStaffCardParameterised("1234", null));
     }
 
+    @Test
+    public void testValidatePhoneNumber() {
+        Assert.assertTrue(testValidatePhoneNumberParameterised("", null));
+        Assert.assertTrue(testValidatePhoneNumberParameterised("12345678876543456787654", "Invalid phone number"));
+        Assert.assertTrue(testValidatePhoneNumberParameterised("1234rwe&", "Invalid phone number"));
+        Assert.assertTrue(testValidatePhoneNumberParameterised("0412345678", null));
+        Assert.assertTrue(testValidatePhoneNumberParameterised("+61412345678", null));
+    }
+
     private boolean testValidateStaffCardParameterised(String staffCardID, String expectedError) {
         String error = null;
 
         try {
             Validator.validateStaffCardId(staffCardID);
+        } catch (InvalidInputException e) {
+            error = e.getMessage();
+        }
+
+        if (error == null) {
+            if (expectedError == null) {
+                return true;
+            }
+            return false;
+        }
+
+        return error.equals(expectedError);
+    }
+
+    private boolean testValidatePhoneNumberParameterised(String phone, String expectedError) {
+        String error = null;
+
+        try {
+            Validator.validatePhoneNumber(phone);
         } catch (InvalidInputException e) {
             error = e.getMessage();
         }
