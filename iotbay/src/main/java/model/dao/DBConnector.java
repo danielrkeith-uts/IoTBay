9e88ca06d59fafe9d32ae7721cfe6c0ae1cd0125
danrkeith <daniel.r.keith@student.uts.edu.au>
2025-05-24 16:07:09 +1000

    Removed invalid import

--- iotbay/src/main/java/model/dao/DBConnector.java
+++ iotbay/src/main/java/model/dao/DBConnector.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import model.DB;
-
 public class DBConnector extends DB {
     public DBConnector() throws ClassNotFoundException, SQLException {
         Class.forName(driver);
